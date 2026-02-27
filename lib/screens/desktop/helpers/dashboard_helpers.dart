d7912ad24e532f17fb36e42783b63d8839e086d3
ibrahimmn <ibrahimmn006@gmail.com>
2026-02-27 16:46:27 +0400

    unused impoirt

--- lib/screens/desktop/helpers/dashboard_helpers.dart
+++ lib/screens/desktop/helpers/dashboard_helpers.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/rendering.dart';
+
 import 'package:smooflow/enums/task_status.dart';
 import 'package:smooflow/screens/desktop/constants.dart';
 import 'package:smooflow/screens/desktop/data/design_stage_info.dart';
 
 DesignStageInfo stageInfo(TaskStatus s) {
-  try {
-  return [...kStages, DesignStageInfo(TaskStatus.delivery,  'Delivery',   'Delivery', Color(0xFF2563EB), Color(0xFFDBEAFE))].firstWhere((i) => i.stage == s);
-  } catch(e) {
-    throw "culprit found, status: $s, kstages ln: ${kStages.length}";
-  }
+  // try {
+  // return [...kStages, DesignStageInfo(TaskStatus.delivery,  'Delivery',   'Delivery', Color(0xFF2563EB), Color(0xFFDBEAFE))].firstWhere((i) => i.stage == s);
+  // } catch(e) {
+  //   throw "culprit found, status: $s, kstages ln: ${kStages.length}";
+  // }
+  return kStages.firstWhere((i) => i.stage == s);
 }
 int stageIndex(TaskStatus s) => kStages.indexWhere((i) => i.stage == s);
 String fmtDate(DateTime d) {
