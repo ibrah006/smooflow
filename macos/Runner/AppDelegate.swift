f2c89d74ccdfcd4e19167ee863bfc1b13ab9771a
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-02 16:27:36 +0400

    update

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,3 +1,5 @@
+
+
 import Cocoa
 import FlutterMacOS
 import Sparkle
@@ -8,20 +10,15 @@ class AppDelegate: FlutterAppDelegate {
 
     override func applicationDidFinishLaunching(_ aNotification: Notification) {
         super.applicationDidFinishLaunching(aNotification)
-
-        // Register Flutter plugins
         GeneratedPluginRegistrant.register(with: self)
 
-        // Start Sparkle updater
         updaterController = SPUStandardUpdaterController(
             startingUpdater: true,
             updaterDelegate: nil,
             userDriverDelegate: nil
         )
-        
-        updaterController.updater.feedURL = URL(string: "https://workflow-backend-production.up.railway.app/updates/mac/appcast.xml")
     }
-
+    
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
