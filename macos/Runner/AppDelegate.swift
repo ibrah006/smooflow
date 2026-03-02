3af03df56b0831db90fa31dccdb15ba47b9d499f
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-02 15:28:52 +0400

    sparkle framework added

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
 import Cocoa
 import FlutterMacOS
+import Sparkle
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    var updaterController: SPUStandardUpdaterController!
 
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        super.applicationDidFinishLaunching(aNotification)
+
+        // Register Flutter plugins
+        GeneratedPluginRegistrant.register(with: self)
+
+        // Start Sparkle updater
+        updaterController = SPUStandardUpdaterController(
+            startingUpdater: true,
+            updaterDelegate: nil,
+            userDriverDelegate: nil
+        )
+    }
+
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
 }
