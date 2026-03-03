9a22a63bf9fbad620504e36b2b9c2dc191e31c95
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-03 14:08:54 +0400

    removed sparkle framework added as package in xcode

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,47 +1,44 @@
 import Cocoa
 import FlutterMacOS
-import Sparkle
+// import Sparkle
 import os.log
 
 @main
 class AppDelegate: FlutterAppDelegate {
-//  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-//    return true
-//  }
-//    
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
+ override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+   return true
+ }
+ 
 
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
     
-  var updaterController: SPUStandardUpdaterController!
+//   var updaterController: SPUStandardUpdaterController!
 
   override func applicationDidFinishLaunching(_ aNotification: Notification) {
       print("🔥 App launched")
     
-      updaterController = SPUStandardUpdaterController(
-          startingUpdater: true,
-          updaterDelegate: nil,
-          userDriverDelegate: nil
-      )
+    //   updaterController = SPUStandardUpdaterController(
+    //       startingUpdater: true,
+    //       updaterDelegate: nil,
+    //       userDriverDelegate: nil
+    //   )
         
-      print("🔥 Sparkle initialized")
+    //   print("🔥 Sparkle initialized")
     
-      self.updaterController.updater.checkForUpdates()
+    //   self.updaterController.updater.checkForUpdates()
       
       super.applicationDidFinishLaunching(aNotification)
     // RegisterGeneratedPlugins(registry: self)  <-- Removed, not needed.
   }
     
-    override func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-        return .terminateNow
-    }
+    // override func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
+    //     return .terminateNow
+    // }
     
-    func updaterShouldRelaunchApplication(_ updater: SPUUpdater) -> Bool {
-        // Return true to allow Sparkle to quit and relaunch the app
-        return true
-    }
+    // func updaterShouldRelaunchApplication(_ updater: SPUUpdater) -> Bool {
+    //     // Return true to allow Sparkle to quit and relaunch the app
+    //     return true
+    // }
 }
