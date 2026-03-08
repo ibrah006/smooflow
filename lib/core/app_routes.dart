2d5b18eb5f54acd7a8a037225829163a47d5e160
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-08 16:21:08 +0300

    add desktop viewer screen to routes - wiring it up

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -18,6 +18,7 @@ import 'package:smooflow/screens/desktop/admin_desktop_dashboard.dart';
 import 'package:smooflow/screens/desktop/design_create_task_screen.concept.dart';
 import 'package:smooflow/screens/desktop/design_dashboard.concept.dart';
 import 'package:smooflow/screens/desktop/desktop_printer_management_screen.dart';
+import 'package:smooflow/screens/desktop/desktop_viewer_home_screen.dart';
 import 'package:smooflow/screens/desktop/project_details_screen.dart';
 import 'package:smooflow/screens/desktop/desktop_start_print_job.dart';
 import 'package:smooflow/screens/desktop/task_details_screen.dart';
@@ -458,7 +459,7 @@ class AppRoutes {
         screen = ViewerHomeScreen();
         break;
       case viewerHomeDesktop:
-        screen = Desktop;
+        screen = DesktopViewerHomeScreen();
         break;
       case accountsDashboard:
         screen = AccountsDashboardScreen();
