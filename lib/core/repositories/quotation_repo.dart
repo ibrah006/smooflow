7fc1594aa307d5d05d2467f034852d75299e69bb
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-29 12:04:50 +0300

    catching error from quotation repo functions

--- lib/core/repositories/quotation_repo.dart
+++ lib/core/repositories/quotation_repo.dart
@@ -6,16 +6,23 @@ class QuotationRepo {
   Future<List<Quotation>> getQuotations() async {
     final response = await ApiClient.http.get('/quotations');
     final List<dynamic> data = json.decode(response.body);
+
     return data.map((json) => Quotation.fromJson(json)).toList();
   }
 
   Future<Quotation> getQuotation(String id) async {
     final response = await ApiClient.http.get('/quotations/$id');
+
     return Quotation.fromJson(json.decode(response.body));
   }
 
   Future<Quotation> createQuotation(Map<String, dynamic> data) async {
     final response = await ApiClient.http.post('/quotations', body: data);
+
+    if (response.statusCode != 201) {
+      throw json.decode(response.body)['message'];
+    }
+
     return Quotation.fromJson(json.decode(response.body));
   }
 
@@ -24,6 +31,11 @@ class QuotationRepo {
     Map<String, dynamic> data,
   ) async {
     final response = await ApiClient.http.put('/quotations/$id', body: data);
+
+    if (response.statusCode != 200) {
+      throw json.decode(response.body)['message'];
+    }
+
     return Quotation.fromJson(json.decode(response.body));
   }
 
