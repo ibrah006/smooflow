110fb3caa4f0acae4441bb4555f17f35330dd562
ibrahimmn <ibrahimmn006@gmail.com>
2026-03-29 12:44:47 +0300

    bug fix: calling unknown /quotations instead of /quotation

--- lib/core/repositories/quotation_repo.dart
+++ lib/core/repositories/quotation_repo.dart
@@ -4,20 +4,20 @@ import 'package:smooflow/core/models/quotation.dart';
 
 class QuotationRepo {
   Future<List<Quotation>> getQuotations() async {
-    final response = await ApiClient.http.get('/quotations');
+    final response = await ApiClient.http.get('/quotation');
     final List<dynamic> data = json.decode(response.body);
 
     return data.map((json) => Quotation.fromJson(json)).toList();
   }
 
   Future<Quotation> getQuotation(String id) async {
-    final response = await ApiClient.http.get('/quotations/$id');
+    final response = await ApiClient.http.get('/quotation/$id');
 
     return Quotation.fromJson(json.decode(response.body));
   }
 
   Future<Quotation> createQuotation(Map<String, dynamic> data) async {
-    final response = await ApiClient.http.post('/quotations', body: data);
+    final response = await ApiClient.http.post('/quotation', body: data);
 
     if (response.statusCode != 201) {
       throw json.decode(response.body)['message'];
@@ -30,7 +30,7 @@ class QuotationRepo {
     String id,
     Map<String, dynamic> data,
   ) async {
-    final response = await ApiClient.http.put('/quotations/$id', body: data);
+    final response = await ApiClient.http.put('/quotation/$id', body: data);
 
     if (response.statusCode != 200) {
       throw json.decode(response.body)['message'];
@@ -40,6 +40,6 @@ class QuotationRepo {
   }
 
   Future<void> deleteQuotation(String id) async {
-    await ApiClient.http.delete('/quotations/$id');
+    await ApiClient.http.delete('/quotation/$id');
   }
 }
