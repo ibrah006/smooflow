2ff45db3442668876865c622829eb030c1007dfd
ibrahimmn <ibrahimmn006@gmail.com>
2026-04-06 11:30:26 +0300

    message provider

--- lib/providers/message_provider.dart
+++ lib/providers/message_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smooflow/core/models/message.dart';
 import 'package:smooflow/core/repositories/message_repo.dart';
 import 'package:smooflow/notifiers/message_notifier.dart';
 import 'package:smooflow/states/message.dart';
@@ -10,3 +11,34 @@ final messageNotifierProvider =
       final repo = ref.read(messageRepoProvider);
       return MessageNotifier(repo);
     });
+
+final messageConnectionStatusProvider = StreamProvider<ConnectionStatus>((ref) {
+  final notifier = ref.watch(messageNotifierProvider.notifier);
+  // Create a stream from the TaskNotifier's connection status changes
+  return Stream.periodic(
+    Duration(milliseconds: 500),
+    (_) => notifier.connectionStatus,
+  );
+});
+
+/// Selected task provider
+final selectedTaskProvider = StateProvider<Message?>((ref) => null);
+
+final taskWebSocketClientProvider = Provider<MessageWebSocketClient>((ref) {
+  // Get auth token from your auth provider
+
+  final client = MessageWebSocketClient();
+
+  client.connect();
+
+  ref.onDispose(() {
+    client.dispose();
+  });
+
+  return client;
+});
+
+final taskChangesStreamProvider = StreamProvider<MessageChangeEvent>((ref) {
+  final client = ref.watch(taskWebSocketClientProvider);
+  return client.taskChanges;
+});
