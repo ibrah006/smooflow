2375b7447727af67488cf43ed8dca11820c6e018
ibrahimmn <ibrahimmn006@gmail.com>
2026-04-06 16:56:04 +0300

    do not show notificaiton for 'newMessage' task change event

--- lib/screens/desktop/components/task_list_view.dart
+++ lib/screens/desktop/components/task_list_view.dart
@@ -571,6 +571,8 @@ class _TaskListViewState extends ConsumerState<TaskListView> {
         break;
       case TaskChangeType.nameUpdated:
         return;
+      case TaskChangeType.newMessage:
+        return;
     }
 
     print("[TASK_LIST_VIEW] task: ${event.task?.id}, type: ${event.type}");
