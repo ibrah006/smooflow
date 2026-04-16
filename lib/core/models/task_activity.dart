f6b7427e50303ec74e4be68639169226f58c3156
ibrahimmn <ibrahimmn006@gmail.com>
2026-04-16 10:48:17 +0400

    task activity frontend model

---
+++ lib/core/models/task_activity.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+enum ActivityType {
+  stageForward,
+  stageBackward,
+  printerAssigned,
+  printerUnassigned,
+  taskCreated,
+  taskCompleted,
+  taskCancelled,
+  assigneeAdded,
+  assigneeRemoved,
+  priorityChanged,
+  dueDateChanged,
+  billingStatusChanged,
+}
+
+class TaskActivity {
+  final int id;
+  final ActivityType type;
+  final int taskId;
+  final String taskName;
+  final String? taskDescription;
+  final int taskPriority;
+  final DateTime? taskDueDate;
+  final String taskStatus;
+
+  // Actor (who did this)
+  final String actorId;
+  final String actorName;
+  final String actorInitials;
+  final Color? actorColor;
+
+  // Printer (if relevant)
+  final String? printerId;
+  final String? printerName;
+  final String? printerNickname;
+
+  final Map<String, dynamic>? metadata;
+  final DateTime createdAt;
+  final bool isSeen;
+
+  const TaskActivity({
+    required this.id,
+    required this.type,
+    required this.taskId,
+    required this.taskName,
+    this.taskDescription,
+    required this.taskPriority,
+    this.taskDueDate,
+    required this.taskStatus,
+    required this.actorId,
+    required this.actorName,
+    required this.actorInitials,
+    this.actorColor,
+    this.printerId,
+    this.printerName,
+    this.printerNickname,
+    this.metadata,
+    required this.createdAt,
+    this.isSeen = false,
+  });
+
+  factory TaskActivity.fromJson(Map<String, dynamic> json) {
+    return TaskActivity(
+      id: json['id'],
+      type: _parseActivityType(json['type']),
+      taskId: json['taskId'],
+      taskName: json['taskName'],
+      taskDescription: json['taskDescription'],
+      taskPriority: json['taskPriority'],
+      taskDueDate:
+          json['taskDueDate'] != null
+              ? DateTime.parse(json['taskDueDate'])
+              : null,
+      taskStatus: json['taskStatus'],
+      actorId: json['actorId'],
+      actorName: json['actorName'],
+      actorInitials: json['actorInitials'],
+      actorColor:
+          json['actorColor'] != null
+              ? Color(int.parse(json['actorColor'].replaceFirst('#', '0xFF')))
+              : null,
+      printerId: json['printerId'],
+      printerName: json['printerName'],
+      printerNickname: json['printerNickname'],
+      metadata: json['metadata'],
+      createdAt: DateTime.parse(json['createdAt']),
+      isSeen: json['isSeen'] ?? false,
+    );
+  }
+
+  static ActivityType _parseActivityType(String type) {
+    switch (type) {
+      case 'stage_forward':
+        return ActivityType.stageForward;
+      case 'stage_backward':
+        return ActivityType.stageBackward;
+      case 'printer_assigned':
+        return ActivityType.printerAssigned;
+      case 'printer_unassigned':
+        return ActivityType.printerUnassigned;
+      case 'task_created':
+        return ActivityType.taskCreated;
+      case 'task_completed':
+        return ActivityType.taskCompleted;
+      case 'task_cancelled':
+        return ActivityType.taskCancelled;
+      case 'assignee_added':
+        return ActivityType.assigneeAdded;
+      case 'assignee_removed':
+        return ActivityType.assigneeRemoved;
+      case 'priority_changed':
+        return ActivityType.priorityChanged;
+      case 'due_date_changed':
+        return ActivityType.dueDateChanged;
+      case 'billing_status_changed':
+        return ActivityType.billingStatusChanged;
+      default:
+        return ActivityType.stageForward;
+    }
+  }
+
+  // Helper getters for UI
+  String get fromStage => metadata?['fromStage'] ?? '';
+  String get toStage => metadata?['toStage'] ?? '';
+  String get addedUserName => metadata?['addedUserName'] ?? '';
+  String get removedUserName => metadata?['removedUserName'] ?? '';
+  int? get fromPriority => metadata?['fromPriority'];
+  int? get toPriority => metadata?['toPriority'];
+  String? get fromBillingStatus => metadata?['fromBillingStatus'];
+  String? get toBillingStatus => metadata?['toBillingStatus'];
+}
