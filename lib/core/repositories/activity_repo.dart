34018a102c94ab6d2f7b02012145b60e61d02829
ibrahimmn <ibrahimmn006@gmail.com>
2026-04-16 11:07:54 +0400

    update exception message

--- lib/core/repositories/activity_repo.dart
+++ lib/core/repositories/activity_repo.dart
@@ -8,7 +8,7 @@ class ActivityRepo {
   Future<List<TaskActivity>> fetchInbox() async {
     final response = await ApiClient.http.get('/activities/inbox');
     if (response.statusCode != 200) {
-      throw Exception('Failed to load tasks: ${response.body}');
+      throw Exception('Failed to load inbox: ${response.body}');
     }
 
     final List<dynamic> body = jsonDecode(response.body);
