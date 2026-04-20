ffd892749074e3477ebcfaeecce15b358c29fde2
ibrahimmn <ibrahimmn006@gmail.com>
2026-04-20 17:39:45 +0530

    increased detail panel width to 450

--- lib/screens/desktop/constants.dart
+++ lib/screens/desktop/constants.dart
@@ -26,7 +26,7 @@ class _T {
   static const slate500 = Color(0xFF64748B);
 }
 
-const kDetailWidth = 400.0;
+const kDetailWidth = 450.0;
 
 List<DesignStageInfo> get kStages {
   final isAdmin = LoginService.currentUser!.isAdmin;
