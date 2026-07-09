90c56ed7dedc7054eeb5d1e2b414999665f38298
ibrahimmn <ibrahimmn006@gmail.com>
2026-07-09 17:28:53 +0530

    cleaned up task cache notifier of unnecessary functions

--- lib/notifiers/task_cache_notifier.dart
+++ lib/notifiers/task_cache_notifier.dart
@@ -15,6 +15,9 @@ import 'package:smooflow/states/task.dart';
 
 class TaskCacheNotifier
     extends FamilyNotifier<FilteredTaskCacheState, TaskFilter> {
+  final TaskRepo _repo;
+  late final TaskWebSocketClient _client;
+
   // final Ref ref;
   TaskCacheNotifier(
     this._repo,
@@ -92,16 +95,11 @@ class TaskCacheNotifier
     }
 
     // Trigger UI repaint by dispatching completely renewed state object
-    state = FilteredTaskCache(
+    state = FilteredTaskCacheState(
       totalCounts: state.totalCounts,
       cachedTasks: updatedCache,
     );
   }
-
-  final TaskRepo _repo;
-  late final TaskWebSocketClient _client;
-
-  static const _dataReloadMinInterval = Duration(seconds: 30);
 
   Task? _activeTask;
   bool _loading = false;
@@ -125,191 +123,6 @@ class TaskCacheNotifier
       }
       return false;
     }).toList();
-  }
-
-  loadTaskToMemory(Task task) {
-    state = state.add(task);
-  }
-
-  List<Task> byStatus({required TaskStatus? status}) {
-    return status == null
-        ? state.tasks
-        : state.tasks.where((task) => status == task.status).toList();
-  }
-
-  int countByStatus({required TaskStatus? status}) {
-    return byStatus(status: status).length;
-  }
-
-  List<Task> getTasks({required List<int> taskIds, TaskStatus? taskStatus}) {
-    return state.tasks.where((task) {
-      return taskIds.contains(task.id) &&
-          (taskStatus == null || task.status == taskStatus);
-    }).toList();
-  }
-
-  /// Load all tasks (admin or global list)
-  Future<void> loadAll() async {
-    _loading = true;
-    // What's the purpose of this?
-    // state = [];
-    try {
-      final tasks = await _repo.fetchAllTasks();
-      if (tasks == state.tasks) {
-        // To prevent unessary state updates and widget rebuilds if the fetched tasks are the same as the current state
-        return;
-      }
-      state = state.copyWith(tasks: tasks);
-    } finally {
-      _loading = false;
-    }
-  }
-
-  /// Load only current user’s tasks
-  Future<void> loadMyTasks() async {
-    _loading = true;
-    try {
-      final tasks = await _repo.fetchMyTasks();
-      state = state.copyWith(tasks: tasks);
-    } finally {
-      _loading = false;
-    }
-  }
-
-  Future<Task?> getTaskById(int taskId, {bool forceReload = false}) async {
-    try {
-      // Find the local version of the task (if any)
-      late final Task? localTask = state.taskById(taskId);
-
-      // Prepare timestamps for delta-based sync
-      final updatedAt = !forceReload ? localTask?.updatedAt : null;
-      final activityLogLastModified =
-          !forceReload ? localTask?.activityLogLastModified : null;
-      final assigneeLastAdded =
-          !forceReload ? localTask?.assigneeLastAdded : null;
-
-      if (localTask != null) {
-        final now = DateTime.now();
-        final sampleFuture = now.add(Duration(days: 1));
-
-        if (now.difference(updatedAt ?? sampleFuture) <
-                _dataReloadMinInterval &&
-            now.difference(activityLogLastModified ?? sampleFuture) <
-                _dataReloadMinInterval &&
-            now.difference(assigneeLastAdded ?? sampleFuture) <
-                _dataReloadMinInterval) {
-          // Preventing too frequent calls to server
-          return localTask;
-        }
-      }
-
-      // Call the backend repo function
-      final fetchedTask = await _repo.getTaskById(
-        taskId: taskId,
-        updatedAt: updatedAt,
-        activityLogLastModified: activityLogLastModified,
-        assigneeLastAdded: assigneeLastAdded,
-      );
-
-      // If backend says everything is up-to-date
-      if (fetchedTask == null) {
-        return localTask; // No update needed
-      }
-
-      // Update in-memory state (replace or insert)
-      final updatedList = [...state.tasks];
-
-      final index = updatedList.indexWhere((t) => t.id == taskId);
-      if (index != -1) {
-        updatedList[index].replaceWith(fetchedTask);
-      } else {
-        updatedList.add(fetchedTask);
-      }
-
-      state = state.copyWith(tasks: updatedList);
-
-      return fetchedTask;
-    } catch (e, st) {
-      print('Error loading task by ID: $e\n$st');
-      rethrow;
-    }
-  }
-
-  /// Load all tasks for a given project
-  /// update local tasksLastModifiedAt
-  Future<TasksResponse> loadProjectTasks({
-    required String projectId,
-    bool forceReload = false,
-    // Pass in the local project tasks last modified
-    // This is the datetime of the last modified task of [projectId]
-    required DateTime? projectTasksLastModifiedLocal,
-    // project task Ids (ensure all the ids are included)
-    required List<int> projectTaskIds,
-  }) async {
-    // try {
-    final DateTime? projectTasksLastModifiedServer =
-        !forceReload
-            ? null
-            : await _repo.getProjectTasksLastModified(projectId);
-
-    // Debug
-    // print(
-    //   "tasksLastModifiedAtLocal: ${projectTasksLastModifiedLocal}, update needed: ${projectTasksLastModifiedServer?.isAfter(projectTasksLastModifiedLocal!)}",
-    // );
-
-    final localUpdateNeeded =
-        forceReload && projectTasksLastModifiedLocal != null
-            ? projectTasksLastModifiedServer?.isAfter(
-              projectTasksLastModifiedLocal,
-            )
-            : false;
-
-    late final bool mustGetLogData;
-    if (localUpdateNeeded == true) {
-      mustGetLogData = true;
-    } else {
-      // MUST GET LOCAL DATA (this boolean overrides ensureLatestLogDetails [whether t or f])
-      mustGetLogData =
-          // Although the latest info is not needed, what if the actual tasks are not even loaded into the memory yet?
-          // We can check to see if the reference tasks (IDs) that are in memory (through project model), is having an actual instance of it in memory
-          !projectTaskIds.every(
-            (item) => (state.tasks.map((task) {
-              if (task.projectId == projectId) return task.id;
-            })).toSet().contains(item),
-          );
-    }
-
-    if (localUpdateNeeded == true || mustGetLogData) {
-      final updatedProjectTasks = await _repo.getTasksByProject(
-        projectId,
-        since: mustGetLogData ? null : projectTasksLastModifiedLocal,
-      );
-
-      // Remove the updated tasks from memory (state)
-      final tasksIds = updatedProjectTasks.map((task) => task.id);
-      state.tasks.removeWhere((task) => tasksIds.contains(task.id));
-
-      // Add the updated tasks to memory (state)
-      state = state.copyWith(tasks: updatedProjectTasks);
-
-      return TasksResponse(
-        tasks: updatedProjectTasks,
-        isUpdatedFromDatabase: true,
-        tasksLastModifiedAt: projectTasksLastModifiedServer,
-      );
-    } else {
-      return TasksResponse(
-        tasks:
-            state.tasks.where((task) => task.projectId == projectId).toList(),
-        isUpdatedFromDatabase: false,
-        tasksLastModifiedAt: projectTasksLastModifiedServer,
-      );
-    }
-    // } catch (e) {
-    //   debugPrint("Error loading project tasks,\nerror: $e");
-    //   // state = state.copyWith(error: e.toString());
-    //   return state;
-    // }
   }
 
   bool activeTaskInitialized = false;
@@ -393,28 +206,6 @@ class TaskCacheNotifier
     }
 
     _activeTask = null;
-  }
-
-  /// Fetch today's production team's schedules
-  Future<void> fetchProductionScheduleToday({
-    TaskStatus? status,
-    bool isCompleted = false,
-  }) async {
-    final tasksToday = await _repo.getProductionScheduleToday();
-
-    final tasksTodayIds = tasksToday.map((task) => task.id);
-
-    state.copyWith(
-      tasks:
-          state.tasks
-              .where((task) => !tasksTodayIds.contains(task.id))
-              .toList(),
-    );
-
-    // Update Tasks
-    // state = state.copyWith(
-    //   tasks: tasksToday
-    // );
   }
 
   Future<void> _assignPrinter({
@@ -935,28 +726,6 @@ class TaskCacheNotifier
       _client.unsubscribeFromTask(state.selectedTask!.id);
       state = state.copyWith(selectedTask: null);
     }
-  }
-
-  /// Filter tasks by status
-  List<Task> getTasksByStatus(TaskStatus status) {
-    return state.tasks.where((task) => task.status == status).toList();
-  }
-
-  /// Get tasks sorted by priority
-  List<Task> getTasksByPriority() {
-    final sortedTasks = List<Task>.from(state.tasks);
-    sortedTasks.sort((a, b) => b.priority.compareTo(a.priority));
-    return sortedTasks;
-  }
-
-  /// Get overdue tasks
-  List<Task> getOverdueTasks() {
-    final now = DateTime.now();
-    return state.tasks.where((task) {
-      return task.dueDate != null &&
-          task.dueDate!.isBefore(now) &&
-          task.status != 'completed';
-    }).toList();
   }
 
   /// Clear error
