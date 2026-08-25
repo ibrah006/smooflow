13eb2ce7c9a4348207fbfaab1fbc10a6936c17b2
ibrahimmn <ibrahimmn006@gmail.com>
2026-08-25 11:09:19 +0300

    compensation summary model

---
+++ lib/core/models/attendance/compensation_summary.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'compensation_summary.freezed.dart';
+part 'compensation_summary.g.dart';
+
+@freezed
+class CompensationSummary with _$CompensationSummary {
+  const factory CompensationSummary({
+    required double overtimeHours,
+    required double overtimeAmount,
+    required int weekendDaysCount,
+    required double weekendCompensationHours,
+    required double weekendCompensationAmount,
+    required int holidaysWorkedCount,
+    required double holidayCompensationHours,
+    required double holidayCompensationAmount,
+    required double totalCompensationDue,
+  }) = _CompensationSummary;
+
+  factory CompensationSummary.fromJson(Map<String, dynamic> json) =>
+      _$CompensationSummaryFromJson(json);
+}
